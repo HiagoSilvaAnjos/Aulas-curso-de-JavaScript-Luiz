190afe3fc8c096b6be110498b695ee3931af44d2
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-03-01 11:21:22 -0300

    novos exercicios

---
+++ exercicio-logica/exercicio01.js
@@ -0,0 +1,3 @@
+// TODO: Escrever uma função que recebe 2 numeros e retorna o maior deles
+const maxNumber = (numberOne, numberTwo) => numberOne > numberTwo ? numberOne : numberTwo;
+console.log(maxNumber(10, 15));
---
+++ exercicio-logica/exercicio02.js
@@ -0,0 +1,4 @@
+// TODO: Escreva uma função chamada paisagem que recebe dois argumentos, largura e altura de uma imagem (Number) e Retorne true se a imagem estivr no modo paisagem.
+
+const landscape = (largura, altura) =>largura >= altura;
+console.log(landscape(1080, 1920))
