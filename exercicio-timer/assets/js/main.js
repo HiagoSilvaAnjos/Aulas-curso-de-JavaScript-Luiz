1d6ee0b503fb2e8d79365313683e9133c120fa7b
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-03-02 17:54:54 -0300

    update

--- exercicio-timer/assets/js/main.js
+++ exercicio-timer/assets/js/main.js
@@ -1,43 +1,45 @@
-const clock = document.querySelector('.clock');
-let seconds = 0;
-let timer = null;
+document.addEventListener('DOMContentLoaded', () => {
+    const clock = document.querySelector('.clock');
+    let seconds = 0;
+    let timer = null;
 
-// Criar segundos
-const createTimeOfSeconds = (seconds) => {
-    const data = new Date(seconds * 1000);
+    // Criar segundos
+    const createTimeOfSeconds = (seconds) => {
+        const data = new Date(seconds * 1000);
 
-    return data.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'GMT',
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'GMT',
+        })
+
+    }
+
+    function initializeclock() {
+        timer = setInterval(() => {
+            seconds++;
+            clock.innerHTML = createTimeOfSeconds(seconds);
+            clock.style.color = 'black';
+        }, 1000);
+    }
+
+    document.addEventListener('click', (e) => {
+        const element = e.target;
+
+        if (element.classList.contains('start')) {
+            clearInterval(timer);
+            initializeclock();
+        }
+
+        if (element.classList.contains('stop')) {
+            clock.style.color = 'red';
+            clearInterval(timer);
+        }
+
+        if (element.classList.contains('reset')) {
+            clearInterval(timer);
+            clock.style.color = 'black';
+            clock.innerHTML = '00:00:00'
+            seconds = 0;
+        }
     })
-
-}
-
-function initializeclock() {
-    timer = setInterval(() => {
-        seconds++;
-        clock.innerHTML = createTimeOfSeconds(seconds);
-        clock.style.color = 'black';
-    }, 1000);
-}
-
-document.addEventListener('click', (e) => {
-    const element = e.target;
-
-    if (element.classList.contains('start')) {
-        clearInterval(timer);
-        initializeclock();
-    }
-
-    if (element.classList.contains('stop')) {
-        clock.style.color = 'red';
-        clearInterval(timer);
-    }
-
-    if (element.classList.contains('reset')) {
-        clearInterval(timer);
-        clock.style.color = 'black';
-        clock.innerHTML = '00:00:00'
-        seconds = 0;
-    }
-})
+});
