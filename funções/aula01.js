7de86645a624f4c421fac339daee0b0e993e41bb
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-03-06 16:47:28 -0300

    Update aula01.js

--- funções/aula01.js
+++ funções/aula01.js
@@ -1,6 +1,12 @@
-// Declaração de função
-function falarOi () {
-    return 'OI';
-}
-
+// Declaração de função (Function hoisting)
+function falarOi () {return 'OI';}
 console.log(falarOi());
+
+// First-class objects (Objetos de primeira classe)
+// function expression
+const souUmDado = function () {console.log("Sou um dado")}
+souUmDado();
+
+// Arrow Function
+const funcaoArrow = () => console.log("Sou uma arrow function");
+funcaoArrow();
