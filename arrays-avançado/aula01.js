b14cc0a67dc112f6072adf47d780868dd1183f9d
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-03-09 17:44:47 -0300

    Revisando Arrays

---
+++ arrays-avançado/aula01.js
@@ -0,0 +1,4 @@
+const firstNames = ["Duda", "Hiago", "Igor", "Luiz", "teste"];
+const lastNames = "Carla Silva Oliveira Otávio";
+//                 separa strings em arrays (split);
+const surname = lastNames.split(" ");
