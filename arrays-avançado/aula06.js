69c8d6a085755ad77947e7a8cf8f1ab1b9ffe31a
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-03-11 17:43:56 -0300

    Finalizei a aula sobre reduce

--- arrays-avançado/aula06.js
+++ arrays-avançado/aula06.js
@@ -5,7 +5,7 @@ const numbers = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 const sumNumbers = numbers.reduce((acumulador, valor) => { return acumulador += valor }, 0);
 console.log(sumNumbers);
 
-// TODO: Retorne um array com paresn (filter)
+// TODO: Retorne um array com pares (filter)
 const parNumber = numbers.filter(number => number % 2 === 0);
 console.log(parNumber);
 
@@ -14,15 +14,19 @@ console.log(parNumber);
 const doubleNumbers = numbers.map(number => number * 2);
 console.log(doubleNumbers);
 
-// TODO: Retorne apenas uma strng com o nome da pessoa
-// TODO: Remova apenas a chave nome do objeto
-// TODO: Adicione uma chave id em cada objeto
-
+// TODO: Retorne a pessoa mais velha
 const person = [
     { name: "Luiz", age: 62 },
     { name: "Maria", age: 23 },
     { name: "Ediardo", age: 55 },
     { name: "Letícia", age: 19 },
-    { name: "Rosana", age: 32 },
+    { name: "Rosana", age: 70 },
     { name: "Walace", age: 47 }
 ];
+
+const personOld = person.reduce((accumulator, value) => {
+    if (accumulator.age > value.age) return accumulator;
+    return value;
+})
+
+console.log(personOld)
