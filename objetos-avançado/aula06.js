5e3a39365f99e21905ee124141e40ea9e0e6876a
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-03-14 17:59:29 -0300

    Finalizei a aula sobre heranças

--- objetos-avançado/aula06.js
+++ objetos-avançado/aula06.js
@@ -7,20 +7,33 @@ function Product(name, pricing) {
 Product.prototype.incremento = function (incremento) { return this.pricing += incremento };
 Product.prototype.decremento = function (decremento) { return this.pricing -= decremento };
 
-function Camisa(name, pricing) {
+function Camisa(name, pricing, cor) {
     Product.call(this, name, pricing);
+    this.cor = cor;
 }
 Camisa.prototype = Object.create(Product.prototype);
 Camisa.prototype.constructor = Camisa;
 
-const camisaOrigin = new Camisa("Camiseta", 100)
+const camisaOrigin = new Camisa("Camiseta", 100, "Rosa");
 console.log(camisaOrigin);
 
-function Caneca(name, pricing) {
-    Product.call(this, name, pricing)
+function Caneca(name, pricing, material, stock) {
+    Product.call(this, name, pricing);
+    this.material = material;
+
+    Object.defineProperty(this, "stock", {
+        enumerable: true,
+        configurable: false,
+        get: function () { return stock },
+        set: function (value) {
+            if (typeof value !== "number") throw TypeError("Typeof Value is Invalid");
+            this.stock = value;
+        }
+    })
+
 }
 Caneca.prototype = Object.create(Product.prototype);
 Caneca.prototype.constructor = Caneca;
 
-const canecaOrigin = new Caneca("Porcelana", 30);
-console.log(canecaOrigin);
+const canecaOrigin = new Caneca("Programadores", 30, "Porcelana", 5);
+console.log(canecaOrigin.stock);
