639b0b7e1d64b60e9bad9074f2de8d653b9db624
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-03-22 18:50:26 -0300

    Aula sobre Axios (Outra maneira de usar ajax)

---
+++ javascript-assincrono/javascript-fetch -axios/assets/js/main.js
@@ -0,0 +1,23 @@
+// fetch('pessoas.json')
+//     .then(response => response.json())
+//     .then(json => loadElementsPage(json))
+//     .catch(err => console.error(err));
+
+axios('pessoas.json')
+    .then(response => { loadElementsPage(response.data) })
+    .catch(err => console.log("Not Found"));
+
+function loadElementsPage(dateJson) {
+    dateJson.filter(date => createDivDatesElements(date.nome, date.email, date.cpf));
+}
+
+function createDivDatesElements(name, email, cpf) {
+    const resultDate = document.querySelector('.result');
+    const div = document.createElement('div');
+    div.innerHTML += `<br>`;
+    div.innerHTML += `<p>${name}</p>`;
+    div.innerHTML += `<p>${email}</p>`;
+    div.innerHTML += `<p>${cpf}</p>`;
+
+    resultDate.appendChild(div);
+}
