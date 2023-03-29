bf2ce533cc947ff225e1a909470238e59c405999
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-03-29 19:45:31 -0300

    Update app.js

--- node/aula02/app.js
+++ node/aula02/app.js
@@ -2,4 +2,4 @@ const multiply = require('./mod');
 console.log(multiply(5, 5));
 
 const path = require('path');
-console.log(path.resolve(__dirname, '../../../../'))
+console.log(path.resolve(__dirname, '../../../../'));
