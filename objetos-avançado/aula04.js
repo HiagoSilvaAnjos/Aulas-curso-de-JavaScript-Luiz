2e52299edf509c8235e5ed897efc1a6694d8611e
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-02-11 14:58:25 -0300

    Finalizei a aula sobre PROTOTYPES

--- objetos-avançado/aula04.js
+++ objetos-avançado/aula04.js
@@ -1,18 +1,19 @@
-// Construtora => molde (classe)
+// FIXME: PROTOTYPES
 class Person {
     constructor(name, surname) {
         this.name = name;
         this.surname = surname;
     }
-};
-
-Person.prototype.sayMayName = function () {
-    return `May name is ${this.name} ${this.surname}`
 }
 
-// Instância
-const personOne = new Person("Hiago", "Silva"); // <== pessoa =  função construtora 
-const personTwo = new Person("Luiz", "Otávio"); // <== pessoa =  função construtora 
+// Adiciona um método 'sayMyName' ao protótipo da classe Person
+Person.prototype.sayMyName = function () {
+    return `My name is ${this.name} ${this.surname}`
+};
+
+// Criação de duas instâncias da classe Person
+const personOne = new Person("Hiago", "Silva");
+const personTwo = new Person("Luiz", "Otávio");
 
 console.dir(personOne);
 console.dir(personTwo);
