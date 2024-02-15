b677ff8511d8927c665f13c7c24e2a18ec7e9a0e
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-02-15 07:47:45 -0300

    Update metodos-primisses02.js

--- javascript-assincrono/promisses/metodos-primisses02.js
+++ javascript-assincrono/promisses/metodos-primisses02.js
@@ -1,1 +1,38 @@
 // FIXME: Promisse.race
+/*
+Promise.race é outro método fornecido pelo JavaScript para lidar com Promises, mas com um comportamento um pouco diferente do Promise.all. Enquanto o Promise.all espera por todas as Promises serem resolvidas ou rejeitadas, o Promise.race espera pela primeira Promise ser resolvida ou rejeitada.
+*/
+
+function timerRandom(min, max) {
+    min *= 1000;
+    max *= 1000;
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function conectDataBase(message, timer) {
+    return new Promise((resolve, reject) => {
+
+        if (typeof message !== 'string') {
+            return reject(new TypeError('Message must be a string'))
+        }
+
+        setTimeout(() => {
+            return resolve(message);
+        }, timer);
+
+    })
+}
+
+const promisses = [
+    conectDataBase("API 01...", timerRandom(1, 6)),
+    conectDataBase("API 02...", timerRandom(1, 6)),
+    conectDataBase("API 03...", timerRandom(1, 6)),
+]
+
+Promise.race(promisses)
+    .then(message => {
+        console.log(message);
+    })
+    .catch(err => {
+        console.log(err.message);
+    })
