b0a8a63d13271005d2563d3334f2902c0d586689
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-03-02 18:03:31 -0300

    update and review

--- node_js/aula01/app.js
+++ node_js/aula01/app.js
@@ -1,5 +1,14 @@
 const { Person } = require("./module01");
+const axios = require("axios");
 
 const user = new Person("Hiago", "Silva");
 
 console.log(user.SayMyName());
+
+axios("https://pokeapi.co/api/v2/pokemon?limit=10")
+    .then((response) => {
+        console.log(response.data.results);
+        response.data.results.forEach(item => console.log(item.name));
+    }).catch((err) => {
+        console.log("Error ao buscar");
+    });
